c0a731a884d02bed147b7afa896c2c1246ffb844
gabrielluisnunes <gabrielluisnunes@gmail.com>
2025-11-02 02:48:24 -0300

    Adding Button

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const Home = () => {
+    return (
+        <div>Olá Mundo
+            <Button>Cadastar</Button>
+        </div>
+        
+    )
+}
+
+export default Home
