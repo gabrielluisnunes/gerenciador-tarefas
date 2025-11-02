960f6146a194f43e40ae9b32d7a34669c6f782da
gabrielluisnunes <gabrielluisnunes@gmail.com>
2025-11-02 20:29:52 -0300

    Commit Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 import { Button } from "@/components/ui/button"
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
 
 const Home = () => {
     return (
-        <div>Olá Mundo
+        <div className = "w-lg">
             <Button variant = "outline">Cadastar</Button>
+            <Input />
+            <Card>
+                <CardHeader>
+                    <CardTitle>Card Title</CardTitle>
+                    <CardDescription>Card Description</CardDescription>
+                    <CardAction>Card Action</CardAction>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Card Footer</p>
+                </CardFooter>
+            </Card>
         </div>
         
     )
